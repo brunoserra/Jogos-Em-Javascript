ad516ea1cc391bbc0638153ac903bc0778b181cf
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-03 16:21:29 -0300

    Update jogo.js

--- primeiro jogo/jogo.js
+++ primeiro jogo/jogo.js
@@ -1,12 +1,20 @@
 const jogoDaVelha = {
-    board: ['', '', '', '', '', '', '', '', '', ],
-    simbols: ['x', 'o'],
-    container_element: null,
-    init: function(container) {
-        this.container_element = container
+        board: ['', '', '', '', '', '', '', '', '', ],
+        simbols: ['x', 'o'],
+        container_element: null,
+        init: function(container) {
+            this.container_element = container;
 
+        },
+
+
+        draw: function() {
+            let content = '';
+
+            for (i in this.board)
+                content += '< div >' + i + '</div>';
+
+        },
+        this.container_element.innerHTML = container;
     },
-
-
-    draw: function() {}
-}
+};
