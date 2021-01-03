bd5e8b50df77f7c876f00192c8e48c6eac0d1f37
Bruno Rodrigues Dos Reis Rossi Serra <74634382+brunoserra@users.noreply.github.com>
2021-01-03 15:56:11 -0300

    iniciando

---
+++ primeiro jogo/jogo.js
@@ -0,0 +1,12 @@
+const jogoDaVelha = {
+    board: ['', '', '', '', '', '', '', '', '', ],
+    simbols: ['x', 'o'],
+    container_element: null,
+    init: function(container) {
+        this.container_element = container
+
+    },
+
+
+    draw: function() {}
+}
